10acde4fdcaf5c404e3b89258251af764d2adece
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2023-11-13 17:26:13 -0300

    added footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className=" text-white  mx-10 w-screen font-raj my-10">
+      <p>owner: martinrocio.1992@gmail.com </p>
+      <p>@2023</p>
+    </div>
+  );
 };
