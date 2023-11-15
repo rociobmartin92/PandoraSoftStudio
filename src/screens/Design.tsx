49b700073bdf3dbd9090741ae1b9616b2b05edd5
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2023-11-15 11:44:28 -0300

    added design section

--- src/screens/Design.tsx
+++ src/screens/Design.tsx
@@ -1,3 +1,17 @@
+import des from "../assets/images/des.jpeg";
+
 export const Design = () => {
-  return <div>Design</div>;
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div className="text-white text-center">
+        <p className="text-2xl font-bold mb-4">
+          ¿Tienes una idea y quieres plasmarla en un diseño?
+        </p>
+        <p className="text-lg mb-9">
+          En Pandora Studio te ayudamos en todo el proceso para hacerlo real.
+        </p>
+        <img src={des} className="w-[750px] h-[400px]" alt="Descripción" />
+      </div>
+    </div>
+  );
 };
