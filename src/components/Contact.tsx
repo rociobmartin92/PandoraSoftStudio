fd47b79a5bf873191ba2b6e6e5bf14371072501a
rociobmartin92 <77502194+rociobmartin92@users.noreply.github.com>
2023-11-16 18:02:11 -0300

    bug fix emailjs library and added dontenv

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,7 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import React, { useForm, SubmitHandler } from "react-hook-form";
 import instagra from "../assets/instagram.svg";
 import whatsapp from "../assets/whatsapp.svg";
+import emailjs from "@emailjs/browser";
 
 type IFormInput = {
   name: string;
@@ -12,7 +13,18 @@ type IFormInput = {
   project: string;
 };
 
+interface ITemplate extends HTMLFormElement {
+  from_name: string;
+  from_email: string;
+  message: string;
+  from_state: string;
+  from_project: string;
+  to_name: string;
+}
+
 export const Contact = () => {
+  console.log(import.meta.env.VITE_USER);
+
   const {
     handleSubmit,
     register,
@@ -30,10 +42,38 @@ export const Contact = () => {
   });
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    // e.preventDefault();
     console.log("DATA", data);
-  };
 
-  console.log(errors);
+    const templateData: ITemplate = {
+      from_name: data.name,
+      from_email: data.email,
+      from_state: data.state,
+      from_project: data.project,
+      to_name: "Pandora",
+      message: data.description,
+    };
+
+    console.log("primer paso");
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        templateData,
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log("Entro");
+          console.log(result.text);
+        },
+        (error) => {
+          console.log("No ntro");
+          console.log("Error sending Email", error.text);
+        }
+      );
+    console.log("paso final");
+  };
 
   return (
     <section
@@ -57,9 +97,8 @@ export const Contact = () => {
       <div className="border-b border-gray-900/10 sm:ml-[100px] mt-[40px]">
         <p className="text-2xl sm:mb-5 mb-0 ">Cotizá con nosotros:</p>
         <form
+          // onSubmit={sendEmail}
           onSubmit={handleSubmit(onSubmit)}
-          action="https://us-central1-tu-proyecto-id.cloudfunctions.net/sendEmail" // Reemplaza con la URL de tu función
-          method="POST"
         >
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
@@ -124,10 +163,10 @@ export const Contact = () => {
                   {...register("project", { required: true })}
                   className="pl-3 block w-full rounded-md border-0 py-2 text-black shadow-sm   sm:max-w-xs sm:text-sm sm:leading-6"
                 >
+                  <option value="app">Aplicación Android</option>
                   <option value="we">Web estática</option>
                   <option value="wd">Web dinámica</option>
-                  <option value="app">Aplicación Android</option>
-                  <option value="de">Diseño</option>
+                  <option value="de">Diseños</option>
                 </select>
               </div>
               {errors.project && (
